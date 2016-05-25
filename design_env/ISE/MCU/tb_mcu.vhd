3f7769e41b03d61ecc332571c9479196421af522
Christian Di Battista <christian.dibattista@bluewin.ch>
2016-05-25 20:29:38 +0200

    added rotary encoder signal generation -> thanks to daniw

--- design_env/ISE/MCU/tb_mcu.vhd
+++ design_env/ISE/MCU/tb_mcu.vhd
@@ -12,7 +12,10 @@ architecture TB of tb_mcu is
   signal clk    : std_logic := '0';
   signal Switch : std_logic_vector(3 downto 0);
   signal LED    : std_logic_vector(7 downto 0);
-   
+  signal ROT_A        : std_logic;
+  signal ROT_B        : std_logic;
+  signal ROT_CENTER   : std_logic;
+  
 begin
 
   -- instantiate MUT
@@ -21,7 +24,10 @@ begin
       rst    => rst,
       clk    => clk,
       LED    => LED,
-      Switch => Switch
+      Switch => Switch,
+		ROT_A      => ROT_A,
+      ROT_B      => ROT_B,
+      ROT_CENTER => ROT_CENTER
       );
 
   -- generate reset
@@ -32,6 +38,23 @@ begin
   begin
     wait for 1 sec / CF/2;
     clk <= not clk;
+  end process;
+  
+  -- encoder signal generation
+  p_encoder: process
+  begin
+    ROT_A <= '0';
+    ROT_B <= '0';
+    wait for 1 sec / CF/2 * 5;
+    ROT_A <= '0';
+    ROT_B <= '1';
+    wait for 1 sec / CF/2 * 5;
+    ROT_A <= '1';
+    ROT_B <= '1';
+    wait for 1 sec / CF/2 * 5;
+    ROT_A <= '1';
+    ROT_B <= '0';
+    wait for 1 sec / CF/2 * 5;
   end process;
  
 end TB;
